87972ac9e041911db8e12ef07fe6a40d65c429bd
ck2136 <chong.kim@ucdenver.edu>
2020-01-06 17:06:12 -0800

    Updated script to test PMMSKNN on tug data

--- scripts/test_tug.R
+++ scripts/test_tug.R
@@ -7,15 +7,16 @@
 # Modified by   : ck1
 # }}}
 
-# Load Libraries {{{
+# Load Libraries {{{ ----------------
+
 library("pacman")
-p_load(PMMSKNN, readxl, dplyr, here)
+p_load(PMMSKNN, readxl, dplyr, here,testthat, gamlss, doParallel, future.apply)
 # }}}
 
-# Load Data and Wrangle {{{
-data(tug_full)
+# Load Data and Wrangle {{{ --------------
+tug_full <- readRDS(paste0(here(),"/../MS_Biostats/data/full.RDS"))
 
-## Wrangle Steup {{{
+## Wrangle Steup {{{ -----------------
 # load only the TUG dataset
 full  <- tug_full
 
@@ -39,8 +40,8 @@ full <- full %>%
     filter(!patient_id %in% exclude$patient_id & time < 200)
 
 # Train and Test split for all TKA outcomes: create 
-set.seed(1234)
-full <- PMMSKNN:::baselinemk(full, "patient_id", "time")
+# set.seed(1234)
+# full <- PMMSKNN:::baselinemk(full, "patient_id", "time")
 
 # make sure there aren't any missing data and etc
 summary(full) ;  sapply(full, function(x) {
@@ -52,13 +53,13 @@ full <- full %>%
 
 # }}}
 
-# PREPROCESS DATA {{{
+# PREPROCESS DATA {{{ ---------------
 ## extract fitted values for the linear model (PREOP only here)
 #-- preprocess train and test data involves:
 ###   1. Taking training post data and running broken stick to get y90 or yNU where NU is value chosen
 ###   2. Processes test data set so that yNU is matched with training data
 ###   3. The matched test and train data according to yNU is used to later match the personalized predicted gamlss
-
+#debug(preproc)
 test_proc <- preproc(
                 dff=full,                 # specify full dataset name
                 split_var = 'train_test', # train test split variable
@@ -69,16 +70,47 @@ test_proc <- preproc(
                 outcome = "tug",          # specify outcome variable name
                 time_var = "time",        # specify time variable name
                 pat_id = "patient_id",    # specify patient id variable name
+                m = 20,
                 varlist = c("age","gender","bmi","b_tug"), # specify list of covariates for pmm
                 filter_exp = "time > 3"   # Filter observations that will be included
 )
 
 # }}}
+summary(test_proc$reg_obj)
 
-# RUN LOOCV on Training {{{
 
-# LOOCV: Non Parallel {{{
-fin <- loocv_function(
+# RUN LOOCV on Training with BrokenStick {{{ ----------------------------
+
+res_bs <- loocv_function_bs(
+  # specify number or vector of numbers from {1,...,total number of patients in training data} 
+  nearest_n = seq(10, 30, 10),
+  # enter training and testing post operative and fitted y90 dataset
+  train_post = test_proc$train_post,
+  ord_data = test_proc$train_o,
+  test_post = test_proc$test_post,
+  test_o = test_proc$test_o,
+  # Specify outcome variable and time variable name
+  outcome = "tug",
+  time_elapsed = "time",
+  mtype=0, # Use straight up predicted value
+  # m = 10, # if we're using 
+  # interval = 10,
+  # loocv = FALSE,
+  # userchoose = 9,
+  bs_obj = test_proc$bs_obj,
+  parallel = 7L
+)
+
+res_bs$test_score
+res_bs$loocv_score
+res_bs$nearest_n
+
+# RUN LOOCV on Training {{{ ---------------------
+
+# LOOCV: Non Parallel {{{ ------------------
+
+# undebug(plmout)
+res_ps1 <- loocv_function(
   
   # specify number or vector of numbers from {1,...,total number of patients in training data} 
   nearest_n = c(13:14),
@@ -90,28 +122,30 @@ fin <- loocv_function(
   # Specify outcome variable and time variable name
   outcome = "tug",
   time_elapsed = "time",
-  interval = 10,
-  
+  # interval = 10,
+  mtype=0,
   # Specify use of cubic spline or not
   cs=TRUE,
-  
   # specify degrees of freedom use or not
   dfspec=TRUE,
-  
   # specify degree of freedom for location, scale and shape (d_f_* where * = {m, s} for location and scale default for shape is 1.
   # specify power transformation of location (ptr_m)
   d_f_m=3, ptr_m=0.5,
   d_f_s=1,
-  
   # Specify distribution for location, scale and shape 
   #dist_fam = gamlss.dist::NO)
   dist_fam = gamlss.dist::NO)
 
 # }}}
+res_ps1$loocv_score
+res_ps1$test_score
 
-# LOOCV: Parallel {{{
 
-fin <- loocv_function(
+
+
+# LOOCV: Parallel {{{ -----------------
+
+res_ps2 <- loocv_function(
   
   # specify number or vector of numbers from {1,...,total number of patients in training data} 
   nearest_n = c(35:37),
@@ -125,29 +159,68 @@ fin <- loocv_function(
   #outcome = "knee_flex",
   time_elapsed = "time",
   interval = NULL,
-  
+  mtype=0,
   # Specify use of cubic spline or not
   cs=TRUE,
-  
   # specify degrees of freedom use or not
   #dfspec=NULL,
   dfspec=TRUE,
-  
   # specify degree of freedom for location, scale and shape (d_f_* where * = {m, s} for location and scale default for shape is 1.
   # specify power transformation of location (ptr_m)
   d_f_m=3, ptr_m=0.5,
   #d_f_m=3, ptr_m=1,
   d_f_s=1,
   # parallel
-  parallel=3,
+  parallel=3, m=5,
+  # Specify distribution for location, scale and shape 
+  #dist_fam = gamlss.dist::NO)
+  dist_fam = gamlss.dist::BCCGo)
+
+# }}}
+res_ps2$loocv_score
+res_ps2$test_score
+
+# LOOCV_SKNN -------------------------
+
+
+
+# undebug(pat_level_func_sknn)
+sknnlooObj<- loocv_function_sknn(
+  # specify number or vector of numbers from {1,...,total number of patients in training data} 
+  fulldata = full,
+  nearest_n = c(15,30,45),
+  formula =  ~ age + bmi + b_tug,
+  # enter training and testing post operative and fitted y90 dataset
+  train_post = test_proc$train_post,
+  ord_data = test_proc$train_o,
+  test_post = test_proc$test_post,
+  test_o = test_proc$test_o,
+  # Specify outcome variable and time variable name
+  outcome = "tug",
+  time_elapsed = "time",
+  # interval = 10,
+  
+  # Specify use of cubic spline or not
+  cs=TRUE,
+  
+  # specify degrees of freedom use or not
+  dfspec=TRUE,
+  
+  # specify degree of freedom for location, scale and shape (d_f_* where * = {m, s} for location and scale default for shape is 1.
+  # specify power transformation of location (ptr_m)
+  d_f_m=3, ptr_m=0.5,
+  d_f_s=1,
+  # parallel
+  parallel=5, m=5,
   
   # Specify distribution for location, scale and shape 
   #dist_fam = gamlss.dist::NO)
-  dist_fam = gamlss.dist::NO)
+  dist_fam = gamlss.dist::NO
+)
 
-# }}}
+sknnlooObj$test_score
 
-# test_that() {{{
+# test_that() {{{------------------------
 
 test_that("LOOCV performance list created" , {
              expect_that(names(fin), equals(c("pred_res","loocv_res","loocv_score","nearest_n")))
@@ -158,32 +231,35 @@ test_that("LOOCV performance list created" , {
 
 # }}}
 
-# Plots {{{
+# Plots {{{ ----------------
 
 # Bias, Cov, Pred {{{
-plot_cal(plotobj = fin, test_proc = test_proc, obs_dist = "median")
+plot_cal(plotobj = res_ps1, test_proc = test_proc, obs_dist = "median")
 # }}}
 
 # Calibration {{{
-plot_cal(plotobj = fin, test_proc = test_proc, obs_dist = "median", loocv = FALSE)
+debug(plot_cal)
+plot_cal(plotobj = res_ps1, test_proc = test_proc, obs_dist = "median", loocv = FALSE)
 # }}}
 
 # }}}
 
-# Performance Measures {{{
+# Performance Measures {{{ --------------
 
 # Internal Validation {{{
-loocvperf(fin$loocv_res, 
-          test_proc$train_o, 
-          bias="raw",
-          nearest_n=fin$loocv_score$nearest_n) 
+loocv_perf(
+  res_ps2$loocv_res,
+  outcome="tug",
+  nearest_n=35:37,
+  perf_round_by=4
+)
 # }}}
 
 # External Validation {{{
-extvalid(fin, test_proc)
+extvalid(res_ps2, test_proc)
 # }}}
 
-# Inspection of fin object {{{
+# Inspection of fin object {{{ ----------------
 
 fin %>% str(max.level = 2)
 fin$pred_res$iqrcoverage
