6a8dc4cd90eeb6a18454ed79871c615a8c7b7bf2
ck2136 <chong.kim@ucdenver.edu>
2021-08-06 22:42:32 +0900

    Change extvalid() to show train and test performance and comparison

--- R/extvalid.R
+++ R/extvalid.R
@@ -1,76 +1,42 @@
-#' Calculate external validation measures of bias, coverage and precision
+#' Generate table of training and testing (i.e., external) validation measures of bias, coverage and precision
 #' 
 #' @param loocvobj An object produced by \code{\link{loocv_function}}
-#' @param test_proc An object produced by \code{\link{preproc}}
 #' 
-#' @return A data frame that contains columns of performance measures. The performance measures include difference in prediction (bias), root mean square error (rmse), difference in z-score (zscore), 50% coverage probability (Probability that predictions are included in the inter-quartile range (i.e. 25% to 75%))
+#' @return A data frame that contains columns of performance measures. The performance measures include training and testing set measures of bias, coverage and precision as well as difference in  root mean square error (rmse), 50% coverage probability (Probability that predictions are included in the inter-quartile range (i.e. 25% to 75%)), and precision. The last column 'dominant' displays whether the prediction algorithm performed better in the training set or in the test set.
 #' 
 #' @export
-extvalid <- function(loocvobj,
-                     test_proc){
+extvalid <- function(loocvobj){
   
-  # for matching train and test patients via min value difference in Fitted of train (i.e. pmm)
-  temp <- data.frame(train_id = sapply(1:length(test_proc$test_o$pred), function(x) {
-    #which.min(abs(test_proc$test_o$pred[x] - test_proc$train_o$Fitted))
-    test_proc$train_o[which.min(abs(test_proc$test_o$pred[x] - test_proc$train_o$Fitted)),"id"]
-  }), 
-  test_id = test_proc$test_o$id)
-  precisionvec = test_proc$test_post %>% 
-    dplyr::select(.data$patient_id, .data$time) %>% 
-    rename(test_id = .data$patient_id) %>% 
-    left_join(
-      temp ,
-      by = "test_id"
-    ) %>% 
-    left_join(
-      # merge training data precision by time and training id
-      rbindlist(lapply(loocvobj$pred_res$precisionvec, data.frame), idcol="id")  %>%
-        left_join(
-          # create a table that has ordered training id and sequence from 1 to 398 because the precisionvector doesn't have id's recorded
-          test_proc$train_o %>% dplyr::select(.data$id) %>%
-            rename(train_id = .data$id) %>%
-            cbind(., id=seq(1,nrow(test_proc$train_o))) ,
-          by = "id"
-        ) ,
-      by = c("time","train_id")
-    ) %>%
-    dplyr::select(.data$prec) %>% unlist %>% as.vector
+  res <- loocvobj$loocv_score %>%
+    select(1:5) %>%
+    mutate(train_test = "train") %>%
+    filter(nearest_n == loocvobj$nearest_n) %>%
+    bind_rows(
+      loocvobj$test_score %>%
+        mutate(train_test = "test")
+    )  %>%
+    tidyr::pivot_longer(rmse:prec) %>%
+    select(3:5) %>%
+    tidyr::pivot_wider(names_from =  train_test) %>%
+    mutate(
+      diff = train - test,
+      pdiff = (train - test)/train * 100,
+      dominant = case_when(
+        name == "rmse" & train > test ~ "test",
+        name == "rmse" & train < test ~ "train",
+        name == "rmse" & train == test ~ "tie",
+        
+        name == "cov" & abs(train-0.5) > abs(test-0.5) ~ "test",
+        name == "cov" & abs(train-0.5) < abs(test-0.5) ~ "train",
+        name == "cov" & abs(train-0.5) == abs(test-0.5) ~ "tie",
+        
+        name == "prec" & train > test ~ "test",
+        name == "prec" & train < test ~ "train",
+        name == "prec" & train == test ~ "tie"
+      )
+    )
   
-  res <- data.frame(
-                    bias = mean(abs(loocvobj$pred_res$bias), na.rm=TRUE),
-                    rmse = mean(loocvobj$pred_res$rmse, na.rm=TRUE),
-                    zscore = mean(rbindlist(loocvobj$pred_res$zscore)$zsc[!is.na(rbindlist(loocvobj$pred_res$zscore)$zsc) & !is.infinite(rbindlist(loocvobj$pred_res$zscore)$zsc)]),
-                    coverage = mean(loocvobj$pred_res$iqrcoverage, na.rm=TRUE),
-                    precision = test_proc$test_post %>% 
-                      dplyr::select(.data$patient_id, .data$time) %>% 
-                      rename(test_id = .data$patient_id) %>% 
-                      left_join(
-                        temp ,
-                        by = "test_id"
-                      ) %>% 
-                      left_join(
-                        # merge training data precision by time and training id
-                        rbindlist(lapply(loocvobj$pred_res$precisionvec, data.frame), idcol="id")  %>%
-                          left_join(
-                            # create a table that has ordered training id and sequence from 1 to 398 because the precisionvector doesn't have id's recorded
-                            test_proc$train_o %>% dplyr::select(.data$id) %>%
-                              rename(train_id = .data$id) %>%
-                              cbind(., id=seq(1,nrow(test_proc$train_o))) ,
-                            by = "id"
-                          ) ,
-                        by = c("time","train_id")
-                      )  %>% 
-                      # here we use na.rm but really need to think about if there are any dropped predictions.. probably not a good idea to use the model at all
-                      summarise(precision = mean(.data$prec, na.rm=TRUE))
-  )
-  if(any(is.na(precisionvec))){
-    warning("NA values in precision")
-  }
-  if(any(is.na(rbindlist(loocvobj$pred_res$zscore)$zsc))){
-    warning("NA values in zscore")
-  }
-  if(any(is.na(loocvobj$pred_res$iqrcoverage))){
-    warning("NA values in coverage")
-  }
+
+    
   res
 }
