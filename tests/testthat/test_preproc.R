efed0bfed9f0e4ef1c7abd36f37a0b0a6bd63eb4
ck2136 <chong.kim@ucdenver.edu>
2021-08-10 22:15:14 +0900

    Update preproc() tests and remove unnecessary warnings/stops

--- tests/testthat/test_preproc.R
+++ tests/testthat/test_preproc.R
@@ -41,6 +41,167 @@ test_that(
         Chick = as.numeric(as.character(Chick))
       ) 
     
+    # Create error database with NA values to test 
+    full_nobaseline <- full %>% 
+      dplyr::mutate(baseline_na = NA)
+    
+    
+    expect_error(
+      preproc(
+        dff=full_nobaseline,                 # specify full dataset name
+        split_var = 'train_test', # train test split variable
+        trainval = 1,             # training set value
+        testval = 2,              # test set value
+        knots_exp = c(0, 7, 14, 21), # Specify broken stick knots
+        out_time = 21,            # specify which timepoint to use 
+        outcome = "weight",          # specify outcome variable name
+        time_var = "Time",        # specify time variable name
+        id = "Chick",    # specify patient id variable name
+        baseline_var = "baseline_na",
+        m = 20,
+        varlist = c("b_weight") # specify list of covariates for pmm
+      )
+    )
+    
+    # varlist not populated
+    expect_error(
+      preproc(
+        dff=full,                 # specify full dataset name
+        split_var = 'train_test', # train test split variable
+        trainval = 1,             # training set value
+        testval = 2,              # test set value
+        knots_exp = c(0, 7, 14, 21), # Specify broken stick knots
+        out_time = 21,            # specify which timepoint to use 
+        outcome = "weight",          # specify outcome variable name
+        time_var = "Time",        # specify time variable name
+        id = "Chick",    # specify patient id variable name
+        baseline_var = "baseline",
+        m = 20
+      )
+    )
+    
+    # missing data exists in dff
+    full_na <- full 
+    full_na$b_weight[1] <- NA
+    expect_error(
+      preproc(
+        dff=full_na,                 # specify full dataset name
+        split_var = 'train_test', # train test split variable
+        trainval = 1,             # training set value
+        testval = 2,              # test set value
+        knots_exp = c(0, 7, 14, 21), # Specify broken stick knots
+        out_time = 21,            # specify which timepoint to use 
+        outcome = "weight",          # specify outcome variable name
+        time_var = "Time",        # specify time variable name
+        id = "Chick",    # specify patient id variable name
+        baseline_var = "baseline",
+        m = 20,
+        varlist = c("b_weight") # specify list of covariates for pmm
+      )
+    )
+    
+    # Duplicate baseline values for training and test set
+    full_dupb <- full %>% 
+      bind_rows(
+        full %>% filter(Chick == 1) %>% slice(1) %>% mutate(train_test = 1)
+      )
+    
+    expect_warning(
+      preproc(
+        dff=full_dupb,                 # specify full dataset name
+        split_var = 'train_test', # train test split variable
+        trainval = 1,             # training set value
+        testval = 2,              # test set value
+        knots_exp = c(0, 7, 14, 21), # Specify broken stick knots
+        out_time = 21,            # specify which timepoint to use 
+        outcome = "weight",          # specify outcome variable name
+        time_var = "Time",        # specify time variable name
+        id = "Chick",    # specify patient id variable name
+        baseline_var = "baseline",
+        m = 20,
+        varlist = c("b_weight") # specify list of covariates for pmm
+      )
+    )
+    
+    # duplicate values within trainingset
+    full_dupb <- full %>% 
+      bind_rows(
+        full %>% filter(Chick == 1) %>% slice(1)
+      )
+    
+    expect_warning(
+      preproc(
+        dff=full_dupb,                 # specify full dataset name
+        split_var = 'train_test', # train test split variable
+        trainval = 1,             # training set value
+        testval = 2,              # test set value
+        knots_exp = c(0, 7, 14, 21), # Specify broken stick knots
+        out_time = 21,            # specify which timepoint to use 
+        outcome = "weight",          # specify outcome variable name
+        time_var = "Time",        # specify time variable name
+        id = "Chick",    # specify patient id variable name
+        baseline_var = "baseline",
+        m = 20,
+        varlist = c("b_weight") # specify list of covariates for pmm
+      )
+    )
+    
+    # post op value does not exist
+    full_nopost <- full %>%
+      filter(!(Chick == 1 & baseline == 0))
+    
+    expect_error(
+      preproc(
+        dff=full_nopost,                 # specify full dataset name
+        split_var = 'train_test', # train test split variable
+        trainval = 1,             # training set value
+        testval = 2,              # test set value
+        knots_exp = c(0, 7, 14, 21), # Specify broken stick knots
+        out_time = 21,            # specify which timepoint to use 
+        outcome = "weight",          # specify outcome variable name
+        time_var = "Time",        # specify time variable name
+        id = "Chick",    # specify patient id variable name
+        baseline_var = "baseline",
+        m = 20,
+        varlist = c("b_weight") # specify list of covariates for pmm
+      )
+    )
+    
+    # Use pmmform
+    test_proc <- preproc(
+      dff=full,                 # specify full dataset name
+      split_var = 'train_test', # train test split variable
+      trainval = 1,             # training set value
+      testval = 2,              # test set value
+      knots_exp = c(0, 7, 14, 21), # Specify broken stick knots
+      out_time = 21,            # specify which timepoint to use 
+      outcome = "weight",          # specify outcome variable name
+      time_var = "Time",        # specify time variable name
+      id = "Chick",    # specify patient id variable name
+      baseline_var = "baseline",
+      m = 20,
+      varlist = c("b_weight"), # specify list of covariates for pmm
+      pmmform = "yhat ~ b_weight"
+    )
+    
+    # modelselect = TRUE
+    test_proc <- preproc(
+      dff=full,                 # specify full dataset name
+      split_var = 'train_test', # train test split variable
+      trainval = 1,             # training set value
+      testval = 2,              # test set value
+      knots_exp = c(0, 7, 14, 21), # Specify broken stick knots
+      out_time = 21,            # specify which timepoint to use 
+      outcome = "weight",          # specify outcome variable name
+      time_var = "Time",        # specify time variable name
+      id = "Chick",    # specify patient id variable name
+      baseline_var = "baseline",
+      m = 20,
+      varlist = c("b_weight"), # specify list of covariates for pmm
+      modelselect = TRUE
+    )
+    
+    # Final default case
     test_proc <- preproc(
       dff=full,                 # specify full dataset name
       split_var = 'train_test', # train test split variable
